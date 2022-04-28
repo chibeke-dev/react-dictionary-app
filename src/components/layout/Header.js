85c7042535080550de671c53bd0550ef44c9b990
unknown <chibekee17@gmail.com>
2022-04-28 04:00:27 -0700

    nav updated

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react'
-import { Navbar, NavbarBrand } from 'reactstrap'
+import {Navbar} from 'react-bootstrap'
+import  './Layout.css'
+import logo from '../../Icon.png'
 
 class Header extends Component {
     render() {
         return(
             // Navbar section
-            <Navbar color={'dark'} dark>
-                <NavbarBrand href="/">Dictionary</NavbarBrand>
-            </Navbar>
+            <Navbar className='header' bg='dark' variant='dark'>
+            <div className='navbar__title navbar__item'>
+            <Navbar.Brand href="#">
+                   <img src={logo} 
+                   width="50"
+                  height="30"
+                    alt='Brand logo'
+                    style={{color:"#fff"}}/>
+                    Dictionary
+                  </Navbar.Brand>
+            </div>
+            <div className='navbar__item'>Home</div>
+            <div className='navbar__item'>About</div>
+            <div className='navbar__item'>Contact</div>
+            <div className='navbar__item'>Help</div>         
+        </Navbar>
         )
     }
 
